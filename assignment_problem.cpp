7534a0b91dfb4541ff28b11e62dacabe1953037d
Aman <kushwahaaman2505@gmail.com>
2023-05-24 14:36:42 +0530

    Added the assignment problem

---
+++ assignment_problem.cpp
@@ -0,0 +1,21 @@
+// not completed so far
+#include<bits/stdc++.h>
+using namespace std;
+
+int person, job;
+int costFunctionn(int person,int job,int* costMatrix){
+    /*how to find the minimum cost*/
+    
+}
+
+int main(){
+    int job = 4,person =4;
+     
+    int costMatrix[person][job] = {
+        {9,2,7,8},
+        {6,4,3,7},
+        {5,8,1,8},
+        {7,6,9,4}
+    };
+   // cout<< costFunction()
+}
